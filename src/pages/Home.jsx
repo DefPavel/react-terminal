00c45e49821e3c6f0b1c53f41ff6d3c5c032fd85
DefPavel <strikecool1@gmail.com>
2023-03-09 11:17:08 +0300

    update files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {useState} from "react";
+import { useState } from "react";
 const Home = () => {
 
     const [ImageFiles, setImageFiles] = useState([]);
@@ -7,7 +7,7 @@ const Home = () => {
         try {
             e.preventDefault();
             const formData = new FormData();
-            for(let i =0; i < ImageFiles.length; i++) {
+            for (let i = 0; i < ImageFiles.length; i++) {
                 formData.append("file", ImageFiles[i]);
             }
             const response = await axios({
@@ -30,17 +30,17 @@ const Home = () => {
                     <div className="max-w-2xl mx-auto">
 
                         <label className="block mb-2 font-medium text-gray-900 dark:text-gray-300"
-                               htmlFor="file_input">Загрузка афиш</label>
+                            htmlFor="file_input">Загрузка афиш</label>
                         <input
                             name="uploaded_file"
                             onChange={(e) => setImageFiles(e.target.files)}
                             multiple={true}
                             className="text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                            id="file_input" type="file"/>
+                            id="file_input" type="file" />
                     </div>
                 </div>
                 <button
-                    className="rounded-md bg-indigo-600 py-2 px-3 font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
                     type={"submit"}>
                     Загрузить
                 </button>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,37 +1,84 @@
+import logo from '../assets/logo.svg';
+import { Helmet } from 'react-helmet';
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
 const Login = () => {
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');  
+    const navigate = useNavigate();
+    
+    const SignIn = async (e) => {
+        e.preventDefault();
+
+        navigate('/home');
+    }
+
     return (
-        <div className="w-full max-w-xs">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div className="mb-4">
-                    <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="username">
-                        Логин
-                    </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
-                           required={true}
-                           id="username"
-                           type="text"
-                           placeholder="Введите логин" />
+        <div className="container px-6 py-16 mx-auto">
+            <div className="items-center lg:flex">
+                <div className="w-full lg:w-1/2">
+                    <div className="lg:max-w-lg">
+                        <form className="w-full max-w-md">
+                            <h1
+                                className="mt-4 text-2xl font-semibold tracking-wide text-center text-gray-800 capitalize md:text-4xl dark:text-white">
+                                Admin-<span className="text-blue-500 ">Terminal</span>
+                            </h1>
+                            <div className="relative flex items-center mt-8">
+                                <span className="absolute">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
+                                    </svg>
+                                </span>
+
+                                <input
+                                    onChange={(e) => setLogin(e.target.value)}
+                                    required={true}
+                                    type="text"
+                                    className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                                    placeholder="Введите логин" />
+                            </div>
+
+                            <div className="relative flex items-center mt-4">
+                                <span
+                                    className="absolute">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"
+                                        fill="none" viewBox="0 0 24 24"
+                                        stroke="currentColor" strokeWidth="2">
+                                        <path strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
+                                    </svg>
+                                </span>
+
+                                <input
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required={true}
+                                    type="password"
+                                    className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                                    placeholder="Введите пароль" />
+                            </div>
+
+                            <div className="mt-6">
+                                <button
+                                    className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                                    Ввойти
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <div className="mb-4">
-                    <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="password">
-                        Пароль
-                    </label>
-                    <input className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                           id="password"
-                           type="password"
-                           required={true}
-                           placeholder="Введите пароль" />
+
+                <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+                    <img className="w-full h-full lg:max-w-3xl" src={logo} alt="Catalogue-pana.svg" />
                 </div>
-                <div className="flex items-center justify-between">
-                    <button className="rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            type={"submit"}>
-                        Ввойти
-                    </button>
-                </div>
-            </form>
-            <p className="text-center text-grey text-xs">
-                ©2023
-            </p>
+            </div>
+            <Helmet>
+            <title>Авторизация</title>
+            </Helmet>
         </div>
     )
 }
